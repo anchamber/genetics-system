eecb9176ef780f96745fada9dfadee69d0bd24e6
Rene Schertel <reneschmitt@gmx.de>
2021-04-30 19:46:47 +0200

    test grpc web wrapper

--- main.go
+++ main.go
@@ -5,20 +5,28 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"os"
 
 	"github.com/anchamber/genetics-system/db"
 	pb "github.com/anchamber/genetics-system/proto"
 	"github.com/anchamber/genetics-system/service"
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/grpclog"
 )
 
 var (
-	gRPCPort    = flag.Int("grpc-port", 10000, "The gRPC server port")
-	gatewayPort = flag.Int("gateway-port", 11000, "The gRPC-Gateway server port")
+	enableTls       = flag.Bool("enable_tls", false, "Use TLS - required for HTTP2.")
+	tlsCertFilePath = flag.String("tls_cert_file", "./crt/localhost.crt", "Path to the CRT/PEM file.")
+	tlsKeyFilePath  = flag.String("tls_key_file", "./crt/localhost.key", "Path to the private key file.")
+	gRPCPort        = flag.Int("grpc-port", 10000, "The gRPC server port")
+	gRPCWebPort     = flag.Int("grpc-web-port", 10001, "The gRPC web server port")
 )
 
 func main() {
-	addr := fmt.Sprintf("localhost:%d", *gRPCPort)
+	log.Printf("gRPC port: %d | gRPC web port: %d\n", *gRPCPort, *gRPCWebPort)
+	addr := fmt.Sprintf(":%d", *gRPCPort)
 	lis, err := net.Listen("tcp", addr)
 	if err != nil {
 		log.Fatalln("Failed to listen:", err)
@@ -27,9 +35,55 @@ func main() {
 	pb.RegisterSystemServiceServer(s, service.New(db.NewMockDB(nil)))
 
 	// Serve gRPC Server
-	fmt.Printf("Serving gRPC on https://%s\n", addr)
 	// go func() {
-	// 	log.Fatal(s.Serve(lis))
-	// }()
+	log.Printf("Starting gRPC server %s\n", addr)
 	log.Fatal(s.Serve(lis))
+	// }()
+	// startGRPCWeb()
+	//wrappedServer := grpcweb.WrapServer(s)
+	//handler := func(resp http.ResponseWriter, req *http.Request) {
+	//	wrappedServer.ServeHTTP(resp, req)
+	//}
+	//webAddr := fmt.Sprintf(":%d", *gRPCWebPort)
+	//srv := &http.Server{
+	//	Handler: http.HandlerFunc(handler),
+	//	Addr:    webAddr,
+	//}
+	//// Serve the webapp over TLS
+	//log.Printf("Starting gRPC web server %s\n", webAddr)
+	//log.Fatal(srv.ListenAndServe())
+}
+
+func startGRPCWeb() {
+
+	flag.Parse()
+
+	port := *gRPCWebPort
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSystemServiceServer(grpcServer, service.New(db.NewMockDB(nil)))
+	grpclog.SetLogger(log.New(os.Stdout, "exampleserver: ", log.LstdFlags))
+
+	wrappedServer := grpcweb.WrapServer(grpcServer)
+	handler := func(resp http.ResponseWriter, req *http.Request) {
+		wrappedServer.ServeHTTP(resp, req)
+	}
+
+	httpServer := http.Server{
+		Addr:    fmt.Sprintf(":%d", port),
+		Handler: http.HandlerFunc(handler),
+	}
+
+	grpclog.Printf("Starting server. http port: %d, tls enabled %v", port, *enableTls)
+
+	if *enableTls {
+		if err := httpServer.ListenAndServeTLS(*tlsCertFilePath, *tlsKeyFilePath); err != nil {
+			grpclog.Fatalf("failed starting http2 server: %v", err)
+		}
+	} else {
+		if err := httpServer.ListenAndServe(); err != nil {
+			grpclog.Fatalf("failed starting http server: %v", err)
+		}
+	}
+
 }
